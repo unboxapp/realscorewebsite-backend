65a78658791f5ff445fd7927e4005434ac810342
rajatgrover27 <rajatjayeshgrover@gmail.com>
2025-01-01 16:30:51 +0530

    final deployement

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const Razorpay = require('razorpay');
 const cors = require('cors');
-const fs = require('fs');
 const crypto = require('crypto');
 const axios = require('axios');
 const bodyParser = require('body-parser');
@@ -45,9 +44,11 @@ function toEpochTime() {
   return Math.floor(dt.getTime() / 1000); // Convert to epoch time in seconds
 }
 
-app.get('/fetch-credit-report', async (req, res) => {
+app.post('/fetch-credit-report', async (req, res) => {
   try {
-    const apiUrl = 'https://eb964186-d3c2-48d5-95a4-895797265b4b.mock.pstmn.io/get';
+    const body=req.body;
+    //const apiUrl = 'https://eb964186-d3c2-48d5-95a4-895797265b4b.mock.pstmn.io/get';
+    const apiUrl = 'https://loanunbox.in:443/api/v1/verification/credit_report_checker';
 
     const header = {
       alg: "HS256",
@@ -56,10 +57,8 @@ app.get('/fetch-credit-report', async (req, res) => {
   
     const payload = {
       timestamp: new Date().getTime(), 
-      // timestamp: 1724321625757,
       partnerId: "CORP00001490",
       reqid: toEpochTime(), 
-      // reqid: "168682"
     };
   
     const secretKey = process.env.JSON_TOKEN_SECRET_KEY; // Use your key or replace
@@ -81,14 +80,17 @@ app.get('/fetch-credit-report', async (req, res) => {
 
     // API headers
     const headers = {
-      token: token,
-      accept: 'application/json',
-      authorisedkey: "",
-      'content-type': 'application/json',
-      'User-Agent': ""
+      "token": `${token}`,
+      "accept": "application/json",
+      "authorisedkey": `${process.env.PAYSPRINT_AUTHORIZED_KEY}`,
+      "content-type": "application/json",
+      "User-Agent": `${process.env.PAYSPRINT_USER_AGENT}`
     };
 
-    const response = await axios.get(apiUrl, {}, { headers });
+    console.log("Header:-",headers);
+    console.log("Body:- ",req.body);
+
+    const response = await axios.post(apiUrl, body , { headers });
     console.log('API response:', response.data);
     res.status(response.status).json(response.data);
   } catch (error) {
@@ -110,37 +112,12 @@ app.post('/save-customer-details', async(req, res) => {
   try {
     const docRef = db.collection('Users').doc(customerDetails.body.mobile); 
     await docRef.set(customerDetails); 
-    console.log("Check this: ",customerDetails);
+    console.log("Firebase Details: ",customerDetails);
     res.status(200).send({ message: 'Document created successfully' });
   } catch (error) {
     console.error('Error creating document:', error);
     res.status(500).send({ error: 'Failed to create document' });
   }
-});
-
-app.post('/save-report-json', (req, res) => {
-  const newData = req.body; 
-
-  // Path to your JSON file
-  const filePath = './one.json';
-
-  // Write the new data to the file
-  fs.writeFile(filePath, JSON.stringify(newData, null, 2), 'utf8', (err) => {
-    if (err) {
-      return res.status(500).json({ message: 'Error writing file', error: err });
-    }
-    res.status(200).json({ message: 'File replaced successfully', data: newData });
-  });
-});
-
-app.get('/get-report-json', (req, res) => {
-  fs.readFile('one.json', 'utf8', (err, data) => {
-      if (err) {
-          console.error('Error reading JSON file:', err);
-          return res.status(500).json({ error: 'Failed to load credit report' });
-      }
-      res.json(JSON.parse(data));
-  });
 });
 
 // Start the server
